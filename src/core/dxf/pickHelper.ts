8992dbd41b6ba36dd47fac8794a347f5ec23cc73
mahaibing <1427199805@qq.com>
2024-11-08 17:12:17 +0800

    feat(CAD): cad图纸查看时新增鼠标经过的元素辉光

--- src/core/dxf/pickHelper.ts
+++ src/core/dxf/pickHelper.ts
@@ -1,58 +1,53 @@
-import {Raycaster, Mesh, Group, Vector2} from "three";
+import { Raycaster, Mesh, Group, Vector2, Scene, OrthographicCamera } from "three";
 
 export class PickHelper {
     private raycaster: Raycaster;
     private pickedObject: Mesh | Group | undefined;
+    private objects: Mesh[];
+    private camera: OrthographicCamera;
+    private scene: Scene;
+    private bloomLayer: number;
 
-    constructor() {
+    constructor(scene: Scene, camera: OrthographicCamera, bloomLayer: number) {
+        this.camera = camera;
+        this.scene = scene;
+
+        this.bloomLayer = bloomLayer;
+
         this.raycaster = new Raycaster();
+
+        this.objects = [];
+
+        this.scene.traverseVisible(child => {
+            if (child.children.length == 0) {
+                this.objects.push(<Mesh>child);
+            }
+        });
     }
 
-    pick(normalizedPosition: Vector2, scene, camera) {
-        // 如果有被选中的物体，则恢复颜色
+    pick(normalizedPosition: Vector2,) {
+        // 透过截锥投射一道光线
+        this.raycaster.setFromCamera(normalizedPosition, this.camera);
+        // 获取射线相交的物体列表
+        const intersectedObjects = this.raycaster.intersectObjects(this.objects, false);
+        if (intersectedObjects.length) {
+            // 选择第一个对象。这是最近的一个
+            this.select(intersectedObjects[0].object as Mesh);
+        }
+    }
+
+    select(object: Mesh | Group) {
+        if (!object || object.uuid === this.pickedObject?.uuid) return;
+
+        // 如果有被选中的物体，则恢复
         if (this.pickedObject) {
-            this.pickedObject.traverse(child => {
-                // @ts-ignore
-                if (!child.material) return;
-
-                child.layers.set(0);
-
-                // @ts-ignore
-                child.material.color.setHex(child.material.userData.materialColor);
-            })
+            // this.pickedObject.layers.toggle(0);
+            this.pickedObject.layers.toggle(this.bloomLayer);
 
             this.pickedObject = undefined;
         }
 
-        // 透过截锥投射一道光线
-        this.raycaster.setFromCamera(normalizedPosition, camera);
-        // 获取射线相交的物体列表
-        const intersectedObjects = this.raycaster.intersectObjects(scene.children);
-        if (intersectedObjects.length) {
-            // 选择第一个对象。这是最近的一个
-            this.pickedObject = intersectedObjects[0].object as Mesh;
-
-            if (this.pickedObject.parent && this.pickedObject.parent.type !== "Scene") {
-                this.pickedObject = this.pickedObject.parent as Group;
-            }
-
-            this.pickedObject.traverse(child => {
-                // @ts-ignore
-                if (!child.material) return;
-
-                // @ts-ignore 文字(单行/多行文本)不添加辉光效果，会模糊
-                if (child.geometry.type != "TextGeometry" && !child.text) {
-                    child.layers.set(1);
-                }
-
-                // @ts-ignore 存储他原来的颜色
-                child.material.userData = {
-                    // @ts-ignore
-                    materialColor: child.material.color.getHex()
-                };
-                // @ts-ignore 设置其发射颜色为闪烁的黄
-                child.material.color.setHex(0xFFFF00);
-            })
-        }
+        this.pickedObject = object;
+        this.pickedObject.layers.toggle(this.bloomLayer);
     }
 }
